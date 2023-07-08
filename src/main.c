672475e5c53f32f28045640b5cc97d72d34ed311
Beloin Sena <beloin.rodrigues@gmail.com>
2023-07-08 02:38:34 -0300

    Create a small step by step

--- src/main.c
+++ src/main.c
@@ -2,9 +2,15 @@
 // Created by beloin on 28/06/23.
 //
 
-#include "stdio.h"
+#include "game/game.h"
 
 int main() {
-    printf("Hello World\n");
+    Game game;
+    create_game(&game);
+
+    // What to do:
+    //  1. Test calling the connection from another function
+    //  2. Send player information as a protocol.
+    //  3. Simulate the interface using printf();
     return 0;
 }
