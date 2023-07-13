7373a6667cce007742392181bcaf346c227616b9
Beloin Sena <beloin.rodrigues@gmail.com>
2023-07-12 22:06:28 -0300

    Fixing somethings

--- src/main.c
+++ src/main.c
@@ -33,20 +33,6 @@ void *server_worker(void *arg) {
 
 void handle_sigterm(int sig, siginfo_t *sinfo, void *context) {
     should_quit = 1; // Use a semaphore instead?
-    printf("sig:%d\nsinfo->si_pid = %ld\nexit code %d exit reason %d\n", sig,
-           (long) sinfo->si_pid, sinfo->si_status, sinfo->si_code);
-}
-
-void *simplethread(void *arg) {
-    printf("started thread\n");
-    while (1) {
-        if (should_quit) {
-            printf("quitting from simple thread\n");
-            return NULL;
-        }
-        sleep(2);
-        printf("I am running!\n");
-    }
 }
 
 int main(int argc, char **argv) {
@@ -62,7 +48,7 @@ int main(int argc, char **argv) {
     add_player(&game, me);
 
     pthread_t server_thread;
-    err = pthread_create(&server_thread, NULL, simplethread, argv[1]);
+    err = pthread_create(&server_thread, NULL, server_worker, argv[1]);
     if (err != 0) {
         printf("\ncan't create thread :[%s]", strerror(err));
         exit(1);
@@ -73,16 +59,16 @@ int main(int argc, char **argv) {
 
     printf("My PID is %d\n", getpid());
 
+    // This Setup SIGTERM handler.
     struct sigaction act;
     act.sa_sigaction = handle_sigterm;
     sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_SIGINFO; // This sends the information instead of only the signal number
-
+    act.sa_flags = SA_SIGINFO | // This sends the information instead of only the signal number
+                   SA_RESTART;
     if (sigaction(SIGTERM, &act, NULL) == -1) {
         perror("sigaction");
         exit(EXIT_FAILURE);
     }
-    //    (void) signal(SIGTERM, handle_sigterm); // TODO: Would be better to use sigaction
     pause();
 
     if (should_quit) {
