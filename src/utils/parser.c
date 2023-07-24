ffbbe0623e9e1753b0b34dc73a4fd27a56d7be1e
Beloin Sena <beloin.rodrigues@gmail.com>
2023-07-23 22:39:41 -0300

    Trying to parse the addresses into something cool

--- src/utils/parser.c
+++ src/utils/parser.c
@@ -3,12 +3,15 @@
 //
 
 #include <stdint-gcc.h>
+#include "stdlib.h"
 #include "parser.h"
+#include "string.h"
 
 
 static char size = -1;
-static struct Address addresses[100]; // TODO: Create a config file to add this
-static char *port;
+static char *port; // Server port
+
+static struct Address2 addresses[100]; // TODO: Create a config file to add this
 
 /**
  * Args will be `dist_game 3031 localhost:3092 localhost:3093 localhost:3093`
@@ -16,7 +19,7 @@ static char *port;
  * @param argv
  */
 int parse_args(int argc, char **argv) {
-    int i;
+    int i, add_index = 0;
     char *c_arr;
 
     if (argc < 3) {
@@ -27,16 +30,33 @@ int parse_args(int argc, char **argv) {
         c_arr = argv[i];
 
         // While c[x] != '\0'
+        int c_index = 0;
+        uint8_t has_parsed = 0;
+        while (c_arr[c_index] != '\0') {
+            if (c_arr[c_index] == ':') {
+                has_parsed = 1;
+                // Unwrap values
 
-        size++;
+                struct IAddress *address = (struct IAddress *) &addresses[add_index];
+
+
+                // abc:123
+                address->host = (char *) malloc(sizeof(char) * c_index - 1);
+            }
+            c_index++;
+        }
+
+        if (has_parsed) {
+            size++;
+        }
     }
 }
 
-int get_address(struct Address *address) {
+int get_address(struct IAddress **address) { // TODO: Try to send IAddress instead of address
     return size;
 }
 
-int get_port(char **c) {
+int get_server_port(char **c) {
     if (size == -1) { // TODO: There must be a way to use only bitwise operators
         return -1;
     }
--- src/utils/parser.h
+++ src/utils/parser.h
@@ -5,14 +5,30 @@
 #ifndef DISTRIBUTED_SYS_PARSER_H
 #define DISTRIBUTED_SYS_PARSER_H
 
+// TODO: Remember to FREE the port and host?
 struct Address {
-    char *port;
-    char *host;
+    char *arr;
+    unsigned char _portlen; // port_offset = _hostlen
+    unsigned char _hostlen;
+};
+
+struct IAddress { // TODO: Try to extends this
+    void (*get_port)(char *port);
+    void (*get_host)(char *host);
+};
+
+struct Address2 {
+    void (*get_port)(char *port);
+    void (*get_host)(char *host);
+
+    char *arr;
+    unsigned char _portlen;
+    unsigned char _hostlen;
 };
 
 int get_address(struct Address *address);
 
-int get_port(char **c);
+int get_server_port(char **c);
 
 int parse_args(int argc, char **argv);
 
